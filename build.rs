8f68125e0829ff2fbdb97d3f32f1267fcb3cbd9b
Lance Hart <shipsimfan@hotmail.com>
2025-12-28 23:52:18 -0500

    Fixed build.rs for Linux

--- build.rs
+++ build.rs
@@ -1,11 +1,15 @@
-use std::path::PathBuf;
-
-fn main() {
-    let base_path: PathBuf = std::env::var("VULKAN_SDK")
+#[cfg(target_os = "windows")]
+fn add_vulkan_sdk_library() {
+    let base_path: std::path::PathBuf = std::env::var("VULKAN_SDK")
         .expect("Vulkan SDK cannot be found, verify it is installed")
         .into();
 
     let library_path = base_path.join("Lib");
 
     println!("cargo:rustc-link-search=native={}", library_path.display());
+}
+
+fn main() {
+    #[cfg(target_os = "windows")]
+    add_vulkan_sdk_library();
 }
