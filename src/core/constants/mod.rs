bd0add8007a1aefdfa194b922425ae3f8c52718f
Lance Hart <lhart@aagcnc.com>
2026-05-14 11:53:25 -0400

    Add VK_QUEUE_FAMILY_IGNORED

--- src/core/constants/mod.rs
+++ src/core/constants/mod.rs
@@ -1,9 +1,11 @@
 mod bool32;
 mod max_sizes;
 mod null_handle;
+mod queue_families;
 mod versions;
 
 pub use bool32::*;
 pub use max_sizes::*;
 pub use null_handle::*;
+pub use queue_families::*;
 pub use versions::*;
---
+++ src/core/constants/queue_families.rs
@@ -0,0 +1,5 @@
+/// Ignored queue family index sentinel
+///
+/// The special queue family index [`VK_QUEUE_FAMILY_IGNORED`] indicates that a queue family
+/// parameter or member is ignored.
+pub const VK_QUEUE_FAMILY_IGNORED: u32 = !0;
